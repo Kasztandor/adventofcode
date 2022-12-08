4863e4b9c7e7a6a2868d30656c0ccb18f14baa97
Michał Swoboda <michal.swoboda@interia.pl>
2022-12-08 18:09:09 +0100

    day 8 part 2 some code

--- 2022/day8/part2.py
+++ 2022/day8/part2.py
@@ -1,19 +1,32 @@
-f = open("input.txt")
+f = open("day8/inp.txt")
 lines = []
 visible = []
 def howManyTrees(x, y):
-    trees = 1
-    treeSize = lines[x][y]
+    trees = 0
+    #treeSize = lines[x][y]
     for i in [[1,0],[-1,0],[0,-1],[0,1]]:
-        treeSize = lines[x][y]
+        xx = x
+        yy = y
+        biggestTreeSize = 0
+        while True:
+            xx += i[0]
+            yy += i[1]
+            if xx < 0 or xx > len(lines)-1 or yy < 0 or yy > len(lines[0])-1:
+                break
+            if int(lines[xx][yy]) >= biggestTreeSize:
+                trees += 1
+                biggestTreeSize = int(lines[xx][yy])
     return trees
 for i in f:
     lines.append(i.rstrip("\n"))
     visible.append([])
     for j in range(len(i.rstrip("\n"))):
         visible[-1].append(howManyTrees(len(visible)-1,j))
-score = 0
+#score = 0
 for i in visible:
     i.sort()
-    score = i[-1]
-print(score)
+    for j in i:
+        print(j, end=" ")
+    print()
+    #score = i[-1]
+#print(score)
