87fb1e007f77802f7b0c6fd63770ef50e38873fc
Kasztandor <michal.swoboda@interia.pl>
2022-12-09 10:24:15 +0100

    day 8

--- 2022/day08/part2.py
+++ 2022/day08/part2.py
@@ -1,32 +1,35 @@
-f = open("day8/inp.txt")
+f = open("input.txt")
 lines = []
 visible = []
 def howManyTrees(x, y):
-    trees = 0
-    #treeSize = lines[x][y]
+    trees = 1
+    treeSize = lines[x][y]
     for i in [[1,0],[-1,0],[0,-1],[0,1]]:
+        biggestTreeSize = -1
+        mul = 0
         xx = x
         yy = y
-        biggestTreeSize = 0
         while True:
             xx += i[0]
             yy += i[1]
             if xx < 0 or xx > len(lines)-1 or yy < 0 or yy > len(lines[0])-1:
                 break
-            if int(lines[xx][yy]) >= biggestTreeSize:
-                trees += 1
-                biggestTreeSize = int(lines[xx][yy])
+            mul += 1
+            if treeSize <= lines[xx][yy]:
+                break
+        trees *= mul
     return trees
 for i in f:
     lines.append(i.rstrip("\n"))
     visible.append([])
     for j in range(len(i.rstrip("\n"))):
-        visible[-1].append(howManyTrees(len(visible)-1,j))
-#score = 0
+        visible[-1].append(0)
+for i in range(len(lines)):
+    for j in range(len(lines[i])):
+        visible[i][j] = howManyTrees(i,j)
+score = -1
 for i in visible:
     i.sort()
-    for j in i:
-        print(j, end=" ")
-    print()
-    #score = i[-1]
-#print(score)
+    if i[-1] > score:
+        score = i[-1]
+print(score)
