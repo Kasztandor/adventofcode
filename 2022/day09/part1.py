56f51cc409f33e79579ef059b6614e24e6294b7c
Michał Swoboda <michal.swoboda@interia.pl>
2022-12-09 22:03:31 +0100

    commit

--- 2022/day09/part1.py
+++ 2022/day09/part1.py
@@ -1,17 +1,17 @@
 f = open("input.txt")
-head = [0,0]
-pp = [0,0] # player position you perv
+line = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
 visited = ["0:0"]
 score = 1
 moves = {"U": [0,1], "D": [0,-1], "L": [-1,0], "R": [1,0]}
 def move(x):
-    ppCopy = pp[:]
-    pp[0] += moves[x][0]
-    pp[1] += moves[x][1]
-    if abs(head[0] - pp[0]) >= 2 or abs(head[1] - pp[1]) >= 2:
-        head[0] = ppCopy[0]
-        head[1] = ppCopy[1]
-        visited.append(str(head[0])+":"+str(head[1]))
+    for i in range(1, len(line)):
+        line[i-1][0] += moves[x][0]
+        line[i-1][1] += moves[x][1]
+        if abs(line[i][0] - line[i-1][0]) >= 2 or abs(line[i][1] - line[i-1][1]) >= 2:
+            line[i][0] = line[i-1][0]-moves[x][0]
+            line[i][1] = line[i-1][1]-moves[x][1]
+            if i == len(line)-1:
+                visited.append(str(line[i][0])+":"+str(line[i][1]))
 for i in f:
     for _ in range(int(i[2:].rstrip("\n"))):
         move(i[0])
